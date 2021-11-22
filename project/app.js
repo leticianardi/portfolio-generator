6088c670342f372eddb76790914b629d9a2178f5
leticianardi <leticia.tnardi@gmail.com>
2021-11-22 14:24:37 -0800

    error

--- project/app.js
+++ project/app.js
@@ -7,7 +7,7 @@ const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile('./project/index.html', generatePage(name, github), err => {
   if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
